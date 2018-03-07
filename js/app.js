fd68424eaa1dcbc5c5568c0fe9086e7afaa64385
Axel VICTORIN <204252@supinfo.com>
2018-03-07 07:46:13 -0400

    Ajout de projets

--- js/app.js
+++ js/app.js
@@ -16,7 +16,25 @@ var ProjectController = function($scope){
     technologies: ["HTML", "CSS", "Bootstrap"],
     imageLink: "img/site1-portfolio.png",
     link: "https://codepen.io/Axoun/full/YNmPer"
-  }]
+  },
+{
+  name: "Site internet de la SMHLM",
+  description: "Dans le cadre de mon stage à la Société Martiniquaise d'HLM, j'ai eu l'occasion" +
+  " de refaire le site internet de la SMHLM. Ce stage m'a permis de renforcer mes compétences en "+
+  "développement Web mais aussi en gestion de projet et en communication.",
+  technologies: ["HTML", "CSS", "Bootstrap", "Joomla!", "JavaScript", "Google Maps API"],
+  imageLink : "img/site3-portfolio.png",
+  link: "http://www.smhlm.org"
+},
+{
+  name: "Outil ETL pour Digicel",
+  description: "Dans le cadre de mon stage à Digicel Antilles-Guyane, j'ai conçu un outil permettant " +
+  "d'automatiser la collecte, la transformation et la mise en base des données générées par les antennes 3G " +
+  "de l'opérateur. Grâce à ce stage, j'ai acquis une meilleur compréhension du développement d'applications " +
+  "d'entreprise.",
+  technologies: ["Python",  "MySQL"],
+  imageLink: "img/site4-portfolio.png" 
+}]
 };
 portfolioApp.controller("projectController", ProjectController);
 
